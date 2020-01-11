6bd6a51b3d7e8d1dcb7fe4ffff727a81b96c8889
Chitresh Kumar <kchit.resh.kumar@gmail.com>
2020-01-11 07:24:18 -0600

    3rd Commit

---
+++ Variables.py
@@ -0,0 +1,4 @@
+Character_name = 'Aghabali'
+print( Character_name + ' was kind and supportive,')
+print('his friend said. And he was very smart and hilarious.')
+print('\"He always cracked you up in any conversation," Kaveh said. "We cannot believe he is gone."')
