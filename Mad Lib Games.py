2c991250ed4ae34b432540ade05f170587812ecd
Chitresh Kumar <kchit.resh.kumar@gmail.com>
2020-01-11 07:53:33 -0600

    6th Commit

---
+++ Mad Lib Games.py
@@ -0,0 +1,9 @@
+color = input("Enter a color : ")
+Plural_noun = input("Enter a Plural Noun:")
+celebrity = input("Enter a celebrity :")
+
+
+
+print("Roses are " + color)
+print(Plural_noun +" are blue")
+print("I love " + celebrity)
