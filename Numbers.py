c7cce814a41e24bffd8b82cb675d5c2beed6442d
Chitresh Kumar <kchit.resh.kumar@gmail.com>
2020-01-11 07:33:25 -0600

    4th Commit

---
+++ Numbers.py
@@ -0,0 +1,4 @@
+from math import *
+my_num = -5.8
+print(round(my_num))
+print((sqrt(36)))
--- Variables.py
+++ Variables.py
@@ -2,3 +2,8 @@ Character_name = 'Aghabali'
 print( Character_name + ' was kind and supportive,')
 print('his friend said. And he was very smart and hilarious.')
 print('\"He always cracked you up in any conversation," Kaveh said. "We cannot believe he is gone."')
+print('Chitresh\" Kumar')
+phrase = "Chitresh Kumar"
+print(phrase[0])
+print(phrase.index("Kumar"))
+print(phrase.replace("Chitresh","Ankit"))
