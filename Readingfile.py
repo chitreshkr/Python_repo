c209009813ddb033b7f9a008373907424e1d48a8
Chitresh Kumar <kchit.resh.kumar@gmail.com>
2020-01-19 13:41:12 -0600

    19th Commit

---
+++ Readingfile.py
@@ -0,0 +1,9 @@
+x = open("a.text",'r') #read the file
+open("a.text",'w') #write the file
+open("a.text",'a') #append the file
+open("a.text",'r+') #read and write
+
+x.readline()
+x.close()
+
+
---
+++ module.py
@@ -0,0 +1,3 @@
+import docx as docx
+
+docx.Document()
