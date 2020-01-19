c2615d94a6c772cab5a701b9113da46dc274b09f
Chitresh Kumar <kchit.resh.kumar@gmail.com>
2020-01-18 20:58:25 -0600

    17th Commit

--- list2d.py
+++ list2d.py
@@ -1,4 +1,8 @@
 number_grid = [[1,2,3],
                [4,5,6],
                [7,8,9]]
-print(number_grid[2][2])
+#print(number_grid[2][2])
+
+for row in number_grid:
+    for col in row:
+        print(col)
