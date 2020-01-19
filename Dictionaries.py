1c6557606808ed48d8c3511531696a7755c54d1f
Chitresh Kumar <kchit.resh.kumar@gmail.com>
2020-01-18 19:06:35 -0600

    12th Commit

---
+++ Dictionaries.py
@@ -0,0 +1,11 @@
+monthConversions = {
+    'Jan' : 'January',
+    'Feb' : 'February',
+    'Mar' : 'March',
+    'Apr' : 'April',
+    'May' : 'May',
+    'Jun' : 'June'
+}
+
+#print(monthConversions['Jan'])
+print(monthConversions.get('Nov','Not a valid key'))
