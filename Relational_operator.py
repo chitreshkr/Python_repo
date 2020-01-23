7217c697f4ffe55c6ec9b5de54ce4bf1a08e880a
Chitresh Kumar <kchit.resh.kumar@gmail.com>
2020-01-22 20:43:30 -0600

    23th Commit

---
+++ Relational_operator.py
@@ -0,0 +1,21 @@
+# Examples of Relational Operators
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+# a > b is False
+print(a > b)
+
+# a < b is True
+print(a < b)
+
+# a == b is False
+print(a == b)
+
+# a != b is True
+print(a != b)
+
+# a >= b is False
+print(a >= b)
+
+# a <= b is True
+print(a <= b)
---
+++ mathfunctions.py
@@ -0,0 +1,22 @@
+a  = int(input("Enter a number"))
+b  = int(input("Enter a number"))
+
+#sum of the number
+sum = a + b
+
+#Difference of the number
+Diff = a - b
+
+#Multiplication of the number
+mul = a * b
+
+#Division of the number
+Div = a/b
+
+#Modulus
+rem = a%b
+print(sum)
+print(Diff)
+print(mul)
+print(Div)
+print(rem)
