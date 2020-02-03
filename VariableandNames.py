1bbc8dfb2c60bafe7a7442b06a00ebe3b26b907f
Chitresh Kumar <8807772+chitreshkr@users.noreply.github.com>
2020-02-02 19:04:52 -0600

    27th Commit

---
+++ VariableandNames.py
@@ -0,0 +1,13 @@
+my_name = 'Chitresh Kumar'
+my_age = 26
+my_height = 181
+my_weight = 156
+my_eyes = 'brown-black'
+my_teeth = 'white'
+my_hair = 'Black'
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} cm tall.")
+print(f"He weighs {my_weight} pounds")
+
+total = my_age + my_weight + my_height
+print(f"If I add age {my_age} , height {my_height} and weight {my_weight} the sum would be {total}.")
