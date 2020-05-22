19e86c8c764c85946d4ce98a7c6f0bac1bef143f
Chitreshkr <kchit.resh.kumar@gmail.com>
2020-05-22 09:58:45 -0500

    41 commit

---
+++ Parameters.py
@@ -0,0 +1,9 @@
+#parameters
+def say_hello(name,emoji):
+    print(f"hello {name} {emoji}")
+#arguments
+say_hello('Chitresh','😋')
+
+#positional arguments
+#keyword argument
+say_hello(emoji='😀',name='Chitresh')
--- Range.py
+++ Range.py
@@ -3,5 +3,5 @@ list1 = []
 for num in range(0,100):
     list1.append(num)
 print(list1)
-#for num in range(0,2):
-#    print(list(range(0,10)))
+for num in range(0,2):
+    print(list(range(0,10)))
