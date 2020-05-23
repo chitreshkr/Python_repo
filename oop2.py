ef9abd5aa41b3a3ec777046d1c26261a0fcf2f3f
Chitreshkr <kchit.resh.kumar@gmail.com>
2020-05-22 23:49:54 -0500

    42 commit

--- oop2.py
+++ oop2.py
@@ -1,22 +1,22 @@
 #OOP
 class PlayerCharacter:
     #class object attribute
-    membership = False
+    membership = True
     def __init__(self,name,age):
         if (self.membership):
-            self.name = name
+            self.name = name                    #attributes
             self.age = age
-            pass
 
-    def run(self):
-        print('run')
-        return 'done'
+
+    def shout(self):
+        print(f'My name is {self.name}')
+
 
 player1 = PlayerCharacter('Chitresh',26)
 player2 = PlayerCharacter('Cindy',44)
 
-print(player1.name,player1.age)
+#print(player1.name,player1.age)
 #print(player2.name,player2.age)
-#print(player1.run())
+print(player1.shout())
 #help(list)
-print(player2.membership)
+#print(player2.membership)
